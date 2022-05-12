aa26591a24b0b02dbc8c9920887aa93bc8b0ac0b
Arjun31415 <arjunp0710@gmail.com>
2022-05-12 22:03:12 +0530

    inital raycasting

--- src/include/Player.cpp
+++ src/include/Player.cpp
@@ -1,8 +1,16 @@
 #include "Player.hpp"
 #include "debug/debug.hpp"
+#include <SFML/Graphics/PrimitiveType.hpp>
+#include <SFML/Graphics/Vertex.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <vector>
+inline namespace Object
+{
+using sf::Vector2f;
+using sf::Vertex;
 using std::pair;
 using std::unique_ptr;
-
+using std::vector;
 Player::Player()
 {
     x = 0;
@@ -39,6 +47,37 @@ void Player::moveUp() { moveY(-DELTA); }
 void Player::draw(sf::RenderWindow &window)
 {
     unique_ptr<sf::CircleShape> p(new sf::CircleShape(this->r, POINT_COUNT));
-    p->setPosition(this->x, this->y);
+    Vertex origin(Vector2f(this->x, this->y));
+    p->setOrigin(this->x, this->y);
+    vector<Vertex> end_points;
+    for (int i = 0; i < FOV; i += DTHETA)
+    {
+        pair<double, double> end_point;
+        double m = tan(i); // slope of the ray
+        double y1 = this->y, x1 = this->x;
+        vector<pair<double, double>> a = {{y1 / m, y1},
+                                          {-x1, -m * x1},
+                                          {w - x1, m * (w - x1)},
+                                          {(h - y1) / m, h - y1}};
+        for (auto &[x, y] : a)
+        {
+            if (x >= 0 && x <= w && y >= 0 && y <= h)
+            {
+                // this is the endpoint of the ray;
+                end_point = {x, y};
+                double theta = atan(y / x);
+                if (std::abs(theta - i) <= EPSILON)
+                    break;
+            }
+        }
+        end_points.push_back(
+            Vertex(Vector2f(end_point.first + x1, y1 + end_point.second)));
+    }
+    for (auto &e : end_points)
+    {
+        sf::Vertex vertices[] = {origin, e};
+        window.draw(vertices, 2, sf::Lines);
+    }
     window.draw(*p);
 }
+} // namespace Object
--- src/include/Player.hpp
+++ src/include/Player.hpp
@@ -1,13 +1,19 @@
 #pragma once
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <cmath>
 #include <memory>
 #include <vector>
+inline namespace Object
+{
+constexpr const double EPSILON = 1e-7;
 class Player
 {
     double x, y, r;
     constexpr const static double DELTA = 0.5;
     const static unsigned int POINT_COUNT = 50;
+    constexpr const static int FOV = 170;
+    const static unsigned int DTHETA = 10;
     double w, h;
     void moveX(double);
     void moveY(double);
@@ -24,3 +30,4 @@ class Player
     void moveDown();
     void draw(sf::RenderWindow &);
 };
+} // namespace Object
